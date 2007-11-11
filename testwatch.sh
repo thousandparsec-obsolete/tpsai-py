5658cb88291210544c152ca981b6144846dcb131
Tim Ansell <mithro@mithis.com>
2007-11-11 23:04:10 +1030

    A script to watch the status reports from the log files.

---
+++ testwatch.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+for i in 1 2 3 4 5; do 
+	echo -n "ai$i "
+	if grep Exiting ai$i.log; then
+		echo "AI is dead"
+		continue
+	fi
+	number=$((`cat ai$i.log | wc -l` - `grep -n "Status" ai$i.log | sed -e's/.*log://' -e's/:.*//' | tail -1` + 1 )); tail -n $number ai$i.log
+done
